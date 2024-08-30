d4d3fee7493ac053209de4d5b563fd8476b1c861
Yosuke Mori <jolcde75+github@gmail.com>
2024-08-30 23:26:53 +0900

    CSSの当て方

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {InlineStyle} from './components/InlineStyle';
+import {CssModules} from "./components/CssModules";
+import {StyledJsx} from "./components/StyledJsx";
+import {StyledComponents} from "./components/StyledComponents";
+import {Emotion} from "./components/Emotion";
+
+export const App = () => {
+  return (
+    <div className="App">
+        <InlineStyle />
+        <CssModules />
+        <StyledJsx />
+        <StyledComponents />
+        <Emotion />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { Todo } from "./Todo";
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Todo />
+    <App />
   </StrictMode>
 );
