300339784448bf7b017392d18f93cf869d536434
Yosuke Mori <jolcde75+github@gmail.com>
2024-09-06 22:00:18 +0900

    axiosを使ってみる

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import "./style.css"
-import {Router} from "./router/Router";
-import {UserProvider} from "./providers/UserProvider";
+// import {Router} from "./router/Router";
+// import {UserProvider} from "./providers/UserProvider";
+
+import axios  from "axios";
 
 export const App = () => {
-  return (
-      <UserProvider>
-        <Router />
-      </UserProvider>
-  );
+    const onClickUsers = () => {
+        axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+            console.log(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    const onClickUser1 = () => {
+        axios.get("https://jsonplaceholder.typicode.com/users/1").then((res) => {
+            console.log(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    return (
+          <div className={"App"}>
+              <button onClick={onClickUsers}>users</button>
+              <button onClick={onClickUser1}>id=1のuser</button>
+          </div>
+    );
 }
