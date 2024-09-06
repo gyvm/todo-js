73e22e57bfd5a5feb553cdac16126071476e1de8
Yosuke Mori <jolcde75+github@gmail.com>
2024-09-07 02:09:25 +0900

    create new react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import {UserCard} from "./components/UserCard.tsx";
-import { useAllUsers } from './hooks/useAllUsers';
 
 function App() {
-  const { getUsers, userProfiles, loading, error} = useAllUsers()
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className={"App"}>
-        <button onClick={getUsers}>データ取得</button>
-        <br/>
-        {error ? (
-          <p style={{color: "red"}}>データの取得に失敗しました。</p>
-        ) : loading ? (
-          <p>ローディング中</p>
-        ) : (
-          <>
-            {userProfiles.map((user) => (
-              <UserCard key={user.id} user={user} />
-            ))}
-          </>
-        )}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
